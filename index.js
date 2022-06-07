29736f8444b45d7dd7e7725eb59de58c8416bbfb
Kunj <shahkunj42@gmail.com>
2022-06-07 09:19:37 -0400

    commit tree 7c523bb8dd770d74324c988013ef7229a972b6df

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+wrapGifts(gifts);
+
+function writeCards(names, events) {
+    const thankYou = [];
+    for (let i = 0; i < names.length; i++){
+        thankYou.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`); 
+    }
+    return thankYou;
+}
+
+function countDown(number){
+    while (number >= 0) {
+        console.log(number--)
+    }
+}
